6c657daa7a41c5f4e1a102fe567f64fb167695c9
kSenfelds <kr.senfelds@gmail.com>
2020-09-28 22:02:31 +0300

    aa

--- JavaScripttutorial/script.js
+++ JavaScripttutorial/script.js
@@ -16,9 +16,11 @@ function addButton(i) {
     let button = document.createElement("button");
     button.textContent = "BUTTON".concat(i);
     output.append(button);
+    button.setAttribute("data-number", i);
 
     if (i < 42) {
         addButton(i);
+        
     }
     if (i%3==0) {
         button.style.background="green";
@@ -32,9 +34,20 @@ function addButton(i) {
     pluss.append(button2);
     button2.textContent = "+";
     let j = 42;
-    button2.addEventListener("click", function (){addButton(j); j++;})
+    button2.addEventListener("click", function (){ addButton(j); j++;})
+    let minus = document.getElementById("minus");
+    let button3 = document.createElement("button");
+    minus.append (button3);
+    button3.textContent = "-";
+    button3.addEventListener("click", function (){removeButton(j);})
     
+    function removeButton (j) {
+       
+       output.removeChild(j);
+       j--;
+    }
 
+    
  
     /* zaļām pogām uztaisīt tā lai klikšķinot skaitlis palielinās par 1*/
         
